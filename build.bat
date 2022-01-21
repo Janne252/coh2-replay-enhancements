b7c36331befc6dd524833477aa9a6300e598acaf
Janne <janne.a.varjo@gmail.com>
2022-01-21 17:58:57 +0200

    update build script

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 echo "Compiling data.raw.scar..."
-.bin\lua\luac5.1.exe -s -o data.cscar data.raw.scar
+.bin\lua\luac5.1.exe -s -o .\scar\common\data.cscar .\scar\common\data.raw.scar
 
 SET OUTPUT_FILENAME=".\replay-enhancements.zip"
 CALL DEL %OUTPUT_FILENAME%
-.bin\zip\7za.exe u %OUTPUT_FILENAME% LICENSE.txt README.md -i!*.scar -i!*.cscar -x!*.raw.scar
+.bin\zip\7za.exe u %OUTPUT_FILENAME% LICENSE.txt README.md -ir!*.scar -ir!*.cscar -xr!*.raw.scar -x!tests
